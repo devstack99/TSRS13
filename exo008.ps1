acf7bc2c32a08f51ca80a4ebc62b9a0bd37d2c49
devstack99 <belkhaouni.raouf@gmail.com>
2018-04-17 11:06:00 +0200

    Second commit

---
+++ exo008.ps1
@@ -0,0 +1,14 @@
+﻿Param (
+	[Parameter(Mandatory, HelpMessage = "Enter the base for the multiplication table")]
+	[int]$Base,
+	[int]$Max = 12
+)
+cls
+Write-Host
+
+ForEach ($Num in (0 .. $Max))
+{
+	Write-Host "$Base X $Num = $($Base * $Num)"
+}
+
+Write-Host "`n`n`n"
